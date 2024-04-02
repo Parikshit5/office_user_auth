8959d19c602ff03f3e60154398a07fa1ffdff835
parikshit14 <parikshit.purohit@microlent.com>
2024-04-02 14:06:17 +0530

    signup_api validations completed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,17 +14,42 @@ export class UsersService {
   ) { }
   async sign_up(createUserDto: CreateUserDto) {
     try {
-      const name=createUserDto.name;
-      const email=createUserDto.email;
-      const password=createUserDto.password;
-      const mobile_no=createUserDto.mobile_no;
-      const address=createUserDto.address;
+      let existing_email = await this.Users.find({ where: { email: createUserDto.email } });
+      let mobile_no=await this.Users.findOne({ where: { mobile_no: createUserDto.mobile_no } });
 
       
-    } catch (error) {
-      
+    if (existing_email.length) {
+      let msg='Email already exists';
+      return(msg);
     }
-     return await this.Users.save(createUserDto);
+    if(mobile_no){
+      let msg='Mobile number already exist';
+      return(msg);
+    }
+    if(createUserDto.password.length<8){
+      let msg="Password must be of 8 or more characters";
+      return msg;
+    }
+
+    if(createUserDto.email){
+      function isValidEmail(email: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+      if (!isValidEmail(createUserDto.email)) {
+        return ('Please enter a valid email');
+      }
+    }
+
+     
+
+    //Email doesn't exist, proceed with user creation
+    const newUser = await this.Users.save(createUserDto);
+    return newUser;
+
+    } catch (error) {
+      return error;
+    }
   }
 
   findAll() {
