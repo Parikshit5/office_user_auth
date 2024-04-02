510342efcb888625e6e8d9f3a4cbe623023ded92
parikshit14 <parikshit.purohit@microlent.com>
2024-04-02 10:10:21 +0530

    typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:process.env.HOST_NAME,
+      port: Number(process.env.PORT_NAME),
+      username: process.env.USERNAME1,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(process.cwd(),'dist/**/*.entity.js')],
+      synchronize: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
